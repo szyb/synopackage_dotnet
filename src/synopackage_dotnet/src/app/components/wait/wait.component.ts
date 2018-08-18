6c018216808ff9c503831b909726094afe7e8bac
Szymon Belus <sbelus@gmail.com>
2018-08-18 18:45:35 +0200

    add wait component

---
+++ src/synopackage_dotnet/src/app/components/wait/wait.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-wait',
+  templateUrl: './wait.component.html',
+})
+export class WaitComponent {
+ 
+  constructor() {
+    
+  }
+}
