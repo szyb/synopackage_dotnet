077a30a13e7c032af67fd2c27d0b505b4b87681d
Szymon Bełus <sbelus@gmail.com>
2018-08-20 14:47:26 +0200

    user settings (WIP - continue)

--- src/synopackage_dotnet/Model/Services/VersionService.cs
+++ src/synopackage_dotnet/Model/Services/VersionService.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using synopackage_dotnet.Model.DTOs;
 
@@ -9,10 +12,23 @@ namespace synopackage_dotnet.Model.Services
     {
         public IEnumerable<VersionDTO> GetAllVersions()
         {
-            //TODO: implement
-            List<VersionDTO> list = new List<VersionDTO>();
-            list.Add(new VersionDTO() {Version = "6.2"});
-            list.Add(new VersionDTO() {Version = "6.1"});
+            var versionsJson = File.ReadAllText("Config/versions.json");
+            var versions = JsonConvert.DeserializeObject<VersionDTO[]>(versionsJson);
+            Regex regex = new Regex(@"^(?<major>\d)\.(?<minor>\d)(\.(?<micro>\d)){0,1}\-(?<build>(\d){1,5})$");
+            foreach (var version in versions)
+            {
+                Match m = regex.Match(version.Version);
+                if (m.Success)
+                {
+                    version.Major = Convert.ToInt32(m.Groups["major"].Value);
+                    version.Minor = Convert.ToInt32(m.Groups["minor"].Value);
+                    if (!string.IsNullOrWhiteSpace(m.Groups["micro"].Value))
+                        version.Micro = Convert.ToInt32(m.Groups["micro"].Value);
+                    version.Build = Convert.ToInt32(m.Groups["build"].Value);
+                }
+            }
+            var list = versions.ToList();
+            list.Sort();
             return list;
         }
     }
