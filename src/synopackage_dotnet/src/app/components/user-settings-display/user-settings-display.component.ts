a79ef7bf8f4f84425fac59afcdc8ee4535fd26d8
Szymon Bełus <szymon.belus@infover.pl>
2018-08-21 15:08:55 +0200

    add display user settings component

---
+++ src/synopackage_dotnet/src/app/components/user-settings-display/user-settings-display.component.ts
@@ -0,0 +1,42 @@
+import { Injectable, Component, OnChanges } from '@angular/core';
+import { UserSettingsService } from '../user-settings/user-settings.service';
+
+
+@Component({
+  selector: 'app-user-settings-display',
+  templateUrl: './user-settings-display.component.html'
+})
+
+@Injectable()
+export class UserSettingsDisplayComponent {
+  private selectedVersion: string;
+  private selectedModel: string;
+  private selectedIsBeta: boolean;
+  private stableOrBeta: string;
+//  private lastIsSetup: boolean;
+
+  constructor(private userSettingsService: UserSettingsService) {
+    this.userSettingsService.events.subscribe(
+     () => {
+       this.init();
+      }
+    );
+    this.init();
+  }
+
+  init() {
+    this.selectedVersion = this.userSettingsService.getUserVersion();
+    this.selectedModel = this.userSettingsService.getUserModel();
+    this.selectedIsBeta = this.userSettingsService.getUserIsBeta();
+    if (this.selectedIsBeta) {
+      this.stableOrBeta = 'Beta';
+    } else {
+      this.stableOrBeta = 'Stable';
+    }
+  }
+
+  isSetup(): boolean {
+    return this.userSettingsService.isSetup();
+  }
+
+}
