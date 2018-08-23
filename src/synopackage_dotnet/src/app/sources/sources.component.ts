27a221c3d472dd99f435c43ad8502e4c5ecb4d8d
Szymon Bełus <sbelus@gmail.com>
2018-08-23 14:02:55 +0200

    Browse source - first try (WIP)

--- src/synopackage_dotnet/src/app/sources/sources.component.ts
+++ src/synopackage_dotnet/src/app/sources/sources.component.ts
@@ -1,28 +1,27 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Config } from '../shared/config';
+import { SourceDTO } from './sources.model';
+import { SourcesService } from '../shared/sources.service';
 
 
 @Component({
   selector: 'app-sources',
   templateUrl: './sources.component.html',
 })
+@Injectable()
 export class SourcesComponent {
   public sources: SourceDTO[];
 
-  constructor(http: HttpClient) {
-    http.get<SourceDTO[]>(`${Config.apiUrl}Sources/GetList`).subscribe( result => {
+  constructor(http: HttpClient, private sourcesService: SourcesService) {
+    this.sourcesService.getAllSources().subscribe( result => {
       this.sources = result;
     });
+  }
+    // http.get<SourceDTO[]>(`${Config.apiUrl}Sources/GetList`).subscribe( result => {
+    //   this.sources = result;
+    // });
     // http.get<SourceDTO[]>(Config.apiUrl + 'api/BrowseSource/GetList').subscribe(result => {
     //   this.sources = result;
     // }, error => console.error(error));
-  }
-}
-
-
-interface SourceDTO {
-  name: string;
-  url: string;
-  www: string;
 }
---
+++ src/synopackage_dotnet/src/app/sources/sources.model.ts
@@ -0,0 +1,16 @@
+
+export class SourceDTO {
+  name: string;
+  url: string;
+  www: string;
+}
+
+export class PackageDTO {
+  name: string;
+  ThumbnailUrl: string;
+  Version: string;
+  Package: string;
+  Description: string;
+  IsBeta: boolean;
+  DownloadLink: string;
+}
