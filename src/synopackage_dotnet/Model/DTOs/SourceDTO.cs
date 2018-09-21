62ca577e93e9b39b2eeb86087932503c30bc6b75
Szymon Bełus <sbelus@gmail.com>
2018-09-21 12:04:17 +0200

    fix for converting date from json

--- src/synopackage_dotnet/Model/DTOs/SourceDTO.cs
+++ src/synopackage_dotnet/Model/DTOs/SourceDTO.cs
@@ -1,16 +1,18 @@
 using System;
+using Newtonsoft.Json;
 
 namespace synopackage_dotnet.Model.DTOs
 {
-    public class SourceDTO
-    {
-        public bool Active { get; set; }
-        public string Name {get;set;}
-        public string Url {get;set;}
-        public string Www {get;set;}
-        public string DisabledReason { get; set; }
-        public DateTime? DisabledDate { get; set; }
-        public string CustomUserAgent { get; set; }
+  public class SourceDTO
+  {
+    public bool Active { get; set; }
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public string Www { get; set; }
+    public string DisabledReason { get; set; }
+    [JsonConverter(typeof(DateFormatConverter), "yyyy-MM-dd")]
+    public DateTime? DisabledDate { get; set; }
+    public string CustomUserAgent { get; set; }
 
-    }
+  }
 }
