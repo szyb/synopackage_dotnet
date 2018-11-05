d7939316c2c6f92fe8df1655bc327831c406ccb5
Szymon Bełus <sbelus@gmail.com>
2018-11-05 11:08:08 +0100

    Backend refactor & remove unused code

--- src/synopackage_dotnet/Infrastructure/MapperRegistrator.cs
+++ src/synopackage_dotnet/Infrastructure/MapperRegistrator.cs
@@ -4,18 +4,17 @@ using synopackage_dotnet.Model.SPK;
 
 namespace synopackage_dotnet
 {
-    public static class MapperRegistrator
+  public static class MapperRegistrator
+  {
+    public static void Register()
     {
-        public static void Register()
-        {
-            Mapper.Register<SpkPackage, PackageDTO>()
-                .Member(dest => dest.Name, src => src.Dname != null ? src.Dname : src.Package)
-                .Member(dest => dest.Description, src => src.Desc)
-                .Member(dest => dest.DownloadLink, src => src.Link)
-                .Member(dest => dest.IsBeta, src => src.Beta)
-                //.Member(dest => dest.ThumbnailUrl, src => src.Thumbnail != null ? src.Thumbnail[0] : null)
-                .Member(dest => dest.Version, src => src.Version);
-        }
-        
+      Mapper.Register<SpkPackage, PackageDTO>()
+          .Member(dest => dest.Name, src => src.Dname != null ? src.Dname : src.Package)
+          .Member(dest => dest.Description, src => src.Desc)
+          .Member(dest => dest.DownloadLink, src => src.Link)
+          .Member(dest => dest.IsBeta, src => src.Beta)
+          .Member(dest => dest.Version, src => src.Version);
     }
+
+  }
 }
