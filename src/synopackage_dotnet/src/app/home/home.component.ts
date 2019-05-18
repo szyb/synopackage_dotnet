f938d3444bda2da6c104768a620c186b5b15fec6
Szymon Belus <sbelus@gmail.com>
2019-05-18 09:52:35 +0200

    home page: first version

--- src/synopackage_dotnet/src/app/home/home.component.ts
+++ src/synopackage_dotnet/src/app/home/home.component.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { NewsService } from '../shared/news.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['home.component.css']
 })
 
 export class HomeComponent implements OnInit {
 
+  public keyword: string;
 
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title,
+    private router: Router) {
     this.titleService.setTitle('Home - synopackage.com');
   }
 
   ngOnInit(): void {
   }
 
+  onSearchButton() {
+    this.router.navigate(['/search/keyword', this.keyword]);
+  }
+
+  onBrowseButton() {
+    this.router.navigate(['/sources']);
+  }
+
+  onSettingsButton() {
+    console.log('TODO');
+  }
 }
