6d8507a1ff5f51cc6abd65d841bb11ad099b19ae
Szymon Belus <sbelus@gmail.com>
2019-07-30 21:01:42 +0200

    Info page update

--- src/synopackage_dotnet/src/app/info/info.component.ts
+++ src/synopackage_dotnet/src/app/info/info.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 
 @Component({
@@ -8,4 +9,7 @@ import { Component } from '@angular/core';
 })
 
 export class InfoComponent {
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Info - synopackage.com');
+  }
 }
