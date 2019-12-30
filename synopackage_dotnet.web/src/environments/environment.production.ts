a02e7af992611029a6aaa7902a9faf06ab8880fd
Szymon Bełus <sbelus@gmail.com>
2019-12-30 23:27:03 +0100

    Reorder files
    create new project for model & services
    update dockerfile

---
+++ synopackage_dotnet.web/src/environments/environment.production.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  useGoogleAnalytics: true,
+  googleAnalyticsCode: null,
+  restBaseUrl: null,
+  version: require('../../package.json').version
+};
