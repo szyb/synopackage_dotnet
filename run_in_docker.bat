3e579337130751263b7b3bff9d067415404e55e4
Szymon Bełus <sbelus@gmail.com>
2020-01-09 11:26:00 +0100

    Update run_in_docker.bat

--- run_in_docker.bat
+++ run_in_docker.bat
@@ -1,1 +1,1 @@
-docker run -d -p 8080:80 --name synopackage -e "ASPNETCORE_ENVIRONMENT=docker" -v synopackage_cache:/app/wwwroot/cache synopackage_dotnet
+docker run -d -p 8080:80 --name synopackage -e "ASPNETCORE_ENVIRONMENT=docker" -v synopackage_cache:/app/wwwroot/cache szyb/synopackage_dotnet:latest
