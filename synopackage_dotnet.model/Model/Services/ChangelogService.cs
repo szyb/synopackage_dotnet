718db5b3abd8f3203d4ea709f33eaab177527c5c
Szymon Bełus <sbelus@gmail.com>
2020-11-22 23:02:03 +0100

    Pagination: backend for changelogs

--- synopackage_dotnet.model/Model/Services/ChangelogService.cs
+++ synopackage_dotnet.model/Model/Services/ChangelogService.cs
@@ -1,24 +1,41 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
 using synopackage_dotnet.Model.DTOs;
 
 namespace synopackage_dotnet.Model.Services
 {
-  public class ChangelogService : IChangelogService
+  public class ChangelogService : Paging, IChangelogService
   {
     private readonly string configFile = "Config/changelog.json";
 
-    private IEnumerable<ChangelogDTO> GetChangelogsInternal()
+    private PagingDTO<ChangelogDTO> GetChangelogsInternal(int? page, int? itemsPerPage)
     {
       var changelogJson = File.ReadAllText(configFile);
-      return JsonConvert.DeserializeObject<ChangelogDTO[]>(changelogJson);
+      var changelogs = JsonConvert.DeserializeObject<ChangelogDTO[]>(changelogJson);
+
+      if (page.HasValue && itemsPerPage.HasValue)
+      {
+        var toSkip = GetToSkip(changelogs.Length, page.Value, itemsPerPage.Value);
+
+        PagingDTO<ChangelogDTO> result = new PagingDTO<ChangelogDTO>(
+          GetTotalPages(changelogs.Length, itemsPerPage.Value),
+          page.Value,
+          itemsPerPage.Value,
+          changelogs.Skip(toSkip).Take(itemsPerPage.Value).ToArray());
+        return result;
+      }
+      else
+      {
+        return new PagingDTO<ChangelogDTO>(1, 1, changelogs.Length, changelogs);
+      }
 
     }
 
-    public IEnumerable<ChangelogDTO> GetChangelogs()
+    public PagingDTO<ChangelogDTO> GetChangelogs(int? page, int? itemsPerPage)
     {
-      return GetChangelogsInternal();
+      return GetChangelogsInternal(page, itemsPerPage);
     }
   }
 }
