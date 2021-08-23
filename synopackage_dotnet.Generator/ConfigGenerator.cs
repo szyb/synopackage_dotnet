75597350e096619d1da85375495600b3f0de43fc
Szymon Bełus <sbelus@gmail.com>
2021-08-23 23:08:45 +0200

    Source Generator - sources (WIP)

---
+++ synopackage_dotnet.Generator/ConfigGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace synopackage_dotnet.Generator
+{
+  [Generator]
+  public class ConfigGenerator : ISourceGenerator
+  {
+    public void Execute(GeneratorExecutionContext context)
+    {
+      var files = context.AdditionalFiles.Where(p => p.Path.EndsWith(".json"));
+      foreach (var file in files)
+      {
+        var fileName = Path.GetFileNameWithoutExtension(file.Path);
+        string generatedCode = GenerateClassesFromFile(file.Path);
+
+        context.AddSource($"{fileName}.cs", SourceText.From(generatedCode, Encoding.UTF8));
+
+      }
+    }
+
+    private string GenerateClassesFromFile(string path)
+    {
+      return null;
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+      //no need to initialize
+    }
+  }
+}
---
+++ synopackage_dotnet.Generator/HandlerAbstract.cs
@@ -0,0 +1,26 @@
+﻿namespace synopackage_dotnet.Generator
+{
+  public abstract class HandlerAbstract : IHandler
+  {
+    IHandler next = null;
+
+    public abstract bool CanHandle(string filePath);
+    public virtual string Handle(string filePath)
+    {
+      if (this.next != null)
+      {
+        return this.next.Handle(filePath);
+      }
+      else
+      {
+        return null;
+      }
+    }
+
+    public IHandler SetupNext(IHandler next)
+    {
+      this.next = next;
+      return this.next;
+    }
+  }
+}
