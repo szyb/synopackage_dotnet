f9dc2344378325526a6c9eda9354f69300715bce
Szymon Bełus <sbelus@gmail.com>
2021-08-23 23:08:45 +0200

    Source Generator - sources (WIP)

---
+++ synopackage_dotnet.Generator/IHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace synopackage_dotnet.Generator
+{
+  public interface IHandler
+  {
+    IHandler SetupNext(IHandler next);
+    string Handle(string filePath);
+    bool CanHandle(string filePath);
+  }
+}
---
+++ synopackage_dotnet.Generator/SourcesHandler.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace synopackage_dotnet.Generator
+{
+  public class SourcesHandler : HandlerAbstract
+  {
+    public override bool CanHandle(string filePath)
+    {
+      string fileName = Path.GetFileNameWithoutExtension(filePath);
+      return fileName == "sources";
+    }
+
+    public override string Handle(string filePath)
+    {
+      //string fileName = Path.GetFileNameWithoutExtension(filePath);
+      var content = File.ReadAllText(filePath);
+      dynamic obj = JsonConvert.DeserializeObject(content);
+      return null;
+    }
+  }
+}
