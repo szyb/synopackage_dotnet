ab4b8546a457c8f24c7afb8a85e35ef3a55d0480
Szymon Bełus <sbelus@gmail.com>
2021-08-31 23:23:02 +0200

    Source generator (from sources) (WIP)

---
+++ synopackage_dotnet.Generator/GeneratorHandlerAbstract.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace synopackage_dotnet.Generator
+{
+  public abstract class GeneratorHandlerAbstract : IGeneratorHandler
+  {
+    IGeneratorHandler next = null;
+
+    public abstract bool CanHandle(string filePath);
+    public abstract string ConcreteHandle(string filePath);
+    public string Handle(string filePath)
+    {
+      if (CanHandle(filePath))
+        return this.ConcreteHandle(filePath);
+      if (!CanHandle(filePath) && this.next != null)
+        return this.next.Handle(filePath);
+
+      return null;
+    }
+
+    public IGeneratorHandler SetupNext(IGeneratorHandler next)
+    {
+      this.next = next;
+      return this.next;
+    }
+
+    private static readonly Assembly assembly = typeof(GeneratorHandlerAbstract).Assembly;
+    protected string GetFromResource(string name)
+    {
+      using (Stream stream = assembly.GetManifestResourceStream(name))
+      using (StreamReader reader = new StreamReader(stream))
+      {
+        string result = reader.ReadToEnd();
+        return result;
+      }
+    }
+  }
+}
---
+++ synopackage_dotnet.Generator/SourcesGeneratorHandler.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Scriban;
+using synopackage_dotnet.Generator.Entities;
+using System.Collections.Generic;
+using System.IO;
+
+namespace synopackage_dotnet.Generator
+{
+  public class SourcesGeneratorHandler : GeneratorHandlerAbstract
+  {
+    public override bool CanHandle(string filePath)
+    {
+      string fileName = Path.GetFileNameWithoutExtension(filePath);
+      return fileName == "sources";
+    }
+
+    public override string ConcreteHandle(string filePath)
+    {
+      var content = File.ReadAllText(filePath);
+      var list = JsonConvert.DeserializeObject<IList<SourceDto>>(content);
+      Template template = Template.Parse(GetFromResource("synopackage_dotnet.Generator.Templates.Sources.sbncs"));
+      var rendered = template.Render(new
+      {
+        Sources = list
+      });
+      return rendered;
+    }
+  }
+}
