6dca8e767f97ff182c81a10e4d95befef2ee4307
Szymon Bełus <sbelus@gmail.com>
2022-06-06 20:25:30 +0200

    Add copy url button to pages: browse, search, repository

--- src/Synopackage.Web/src/app/app.component.ts
+++ src/Synopackage.Web/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { NgwWowService } from 'ngx-wow';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'synopackage.com';
