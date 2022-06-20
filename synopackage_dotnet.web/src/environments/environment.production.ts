ba04f2c6683a56dad8c1ab1541957b74d4993334
Szymon Bełus <sbelus@gmail.com>
2022-06-20 22:02:49 +0200

    removed old file

--- synopackage_dotnet.web/src/environments/environment.production.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  useGoogleAnalytics: false,
-  googleAnalyticsCode: null,
-  restBaseUrl: null,
-  version: require('../../package.json').version
-};
